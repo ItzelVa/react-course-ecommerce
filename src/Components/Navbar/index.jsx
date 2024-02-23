fc822be633630b8fdd64a10cccead82a83c34bbd
ItzelVa <itzelochoavallejo@gmail.com>
2024-02-23 09:27:16 -0600

    New feat(sign out): Add an account key and sign-in in localStorage

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -9,6 +9,12 @@ export function Navbar() {
 
     const activeStyle = 'underline underline-offset-4'
 
+    const handleSignOut = () =>{
+        const stringifiedSignOut = JSON.stringify(true)
+        localStorage.setItem('sign-out', stringifiedSignOut)
+        context.setSignOut(true)
+    }
+
     return (
         <nav className="w-full flex justify-between items-center fixed z-10 top-0 py-5 px-8 text-sm font-light">
             <ul className="flex items-center gap-3">
@@ -97,7 +103,8 @@ export function Navbar() {
                     <NavLink 
                     to={'/sign-in'}
                     className={({isActive}) => 
-                        isActive ? activeStyle : undefined}>
+                        isActive ? activeStyle : undefined}
+                    onClick={handleSignOut}>
                         Sign In
                     </NavLink>
                 </li>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,10 +1,40 @@
 import { createContext } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import { json } from "react-router-dom";
 
 export const ShoppingCartContext = createContext()
 
+export const initializeLocalStorage = () => {
+    const accountInLocalStorage = localStorage.getItem('account')
+    const signOutInLocalStorage = localStorage.getItem('sign-out')
+    let parsedAccount
+    let parsedSignOut
+
+    if(!accountInLocalStorage){
+        localStorage.setItem('account',JSON.stringify({}))
+        parsedAccount = {}
+    }
+    else{
+        parsedAccount = JSON.parse(accountInLocalStorage)
+    }
+
+    if(!signOutInLocalStorage){
+        localStorage.setItem('sign-out',JSON.stringify(false))
+        parsedSignOut = false
+    }
+    else{
+        parsedSignOut = JSON.parse(signOutInLocalStorage)
+    }
+}
+
 export function ShoppingCartProvider ({children}) {
+
+    //My account · 
+    const [account, setAccount] = useState({})
+
+    //Sign out · 
+    const [signOut, setSignOut] = useState(false)
 
     //Shoppin cart · Increment quantity
     const [count, setCount] = useState(0)
@@ -75,7 +105,11 @@ export function ShoppingCartProvider ({children}) {
             searchByTitle,
             setSearchByTitle,
             filteredItems,
-            setFilteredItems
+            setFilteredItems,
+            account,
+            setAccount,
+            signOut,
+            setSignOut
         }}>
             {children}
         </ShoppingCartContext.Provider>
